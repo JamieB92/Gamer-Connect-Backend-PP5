9ff31639fa80253c8e1f93c34ef3466a2a42f1fb
JamieB92 <j.bonner6992@gmail.com>
2023-11-15 22:25:16 +0000

    added profile detail view with the ability to edit the profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from profiles import views
 
 
 urlpatterns = [
-    path('profiles/', views.AllProfiles.as_view())
+    path('profiles/', views.AllProfiles.as_view()),
+    path('profiles/<int:pk>', views.ProfileDetail.as_view())
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -5,7 +7,39 @@ from .serializers import ProfileSerializer
 
 
 class AllProfiles(APIView):
+    """
+    Display all profiles
+    Does not create view as profile creation done via django signals
+    """
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Displays an individual profile view
+    """
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
