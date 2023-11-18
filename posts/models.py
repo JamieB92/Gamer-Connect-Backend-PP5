d513bf9873565b0fe2340676aedcb0b293b32986
JamieB92 <j.bonner6992@gmail.com>
2023-11-18 20:33:12 +0000

    create posts app and add serializers

---
+++ posts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """
+    Post model related to the user.
+    User can upload images and videos in a post and will be linked to the users account
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+    header = models.CharField(max_length=255)
+    caption = models.TextField(blank=True)
+    upload = models.FileField(
+        upload_to='files/', default='../default_post_ns8zax', blank=True
+    )
+
+
+    class Meta:
+        ordering = ['-uploaded_on']
+
+    def __str__(self):
+        return f'{self.id} {self.header}'
---
+++ posts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_avatar = serializers.ReadOnlyField(source='owner.profile.profile_avatar.url')
+
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_avatar', 'uploaded_on', 'edited_at',
+            'header', 'caption', 'upload'
+        ]
