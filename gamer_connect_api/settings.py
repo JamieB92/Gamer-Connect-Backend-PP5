d845f9d133dd5a8e4e03574d96fe9e469ed6d740
JamieB92 <j.bonner6992@gmail.com>
2023-11-20 21:51:17 +0000

    like model migration commit

--- gamer_connect_api/settings.py
+++ gamer_connect_api/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'profiles',
     'posts',
     'post_comments',
+    'likes',
     
 ]
 
--- likes/models.py
+++ likes/models.py
@@ -5,18 +5,17 @@ from posts.models import Post
 
 class Like(models.Model):
     """
-    Like model, related to 'owner' and 'post'.
-    'owner' is a User instance and 'post' is a Post instance.
-    'unique_together' makes sure a user can't like the same post twice.
+    Like model associated with 'owner' and 'post', where 'owner' represents a User instance and 'post' represents a Post instance. 
+    The 'unique_together' constraint ensures that a user cannot like the same post more than once.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', on_delete=models.CASCADE
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    liked_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-liked_at']
         unique_together = ['owner', 'post']
 
     def __str__(self):
