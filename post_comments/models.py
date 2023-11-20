94cc9b1e05e7cf5b86f1eb7f3f92efd6c911b0d2
JamieB92 <j.bonner6992@gmail.com>
2023-11-20 19:53:50 +0000

    create post_comments model and add comments serializers

--- post_comments/models.py
+++ post_comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Post comments model related to the User and the Posts created
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commented_at = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+
+    class Meta:
+        ordering = ['-commented_at']
+    def __str__(self):
+        return self.comment
---
+++ post_comments/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class PostCommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the post_Comment model
+    Adds extra fields when listing the comments on a post
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_avatar = serializers.ReadOnlyField(source='owner.profile.profile_avatar.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_avatar',
+            'post', 'created_at', 'updated_at', 'content'
+        ]
