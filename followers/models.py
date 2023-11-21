4b28efd1e86dd59ece20f775b07fe53d759133a6
JamieB92 <j.bonner6992@gmail.com>
2023-11-21 20:58:24 +0000

    create followers model and migration commit

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    Model for followers, involving 'owner' and 'followed' entities.
+    Using unique_togther stops the user from following another user more than once
+    """
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+    started_following = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-started_following']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
