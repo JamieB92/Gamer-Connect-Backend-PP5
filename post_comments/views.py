cf7aa11fab4a24d96b4173a659742dd7291a8643
JamieB92 <j.bonner6992@gmail.com>
2023-11-22 22:02:08 +0000

    add filtering to post comment list

--- post_comments/views.py
+++ post_comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from gamer_connect_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import PostCommentSerializer, PostCommentDetailSerializer
@@ -10,6 +11,12 @@ class PostCommentList(generics.ListCreateAPIView):
     serializer_class = PostCommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+        ]
+    filterset_fields = [
+        'post'
+        ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
