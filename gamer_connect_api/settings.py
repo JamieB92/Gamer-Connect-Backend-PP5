e55913515cf985ec3fa41c66b3564dbc1103ebe0
JamieB92 <j.bonner6992@gmail.com>
2023-11-23 21:20:45 +0000

    set setting for production to set JSON as default

--- gamer_connect_api/settings.py
+++ gamer_connect_api/settings.py
@@ -39,6 +39,11 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
