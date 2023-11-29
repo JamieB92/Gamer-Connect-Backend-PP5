b54b5a44e68a064aa39f0954caeac57dd8a61900
JamieB92 <j.bonner6992@gmail.com>
2023-11-29 22:20:06 +0000

    correct naming in dj_rest_auth serielizer field to fix issue when loggin in

--- gamer_connect_api/serializers.py
+++ gamer_connect_api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_avatar = serializers.ReadOnlyField(source='profile.profile_avatar.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
