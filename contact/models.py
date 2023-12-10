f06daff6695adc053b27f82d672d3c3dda534d46
JamieB92 <j.bonner6992@gmail.com>
2023-12-10 21:33:18 +0000

    create contact model and add to admin panel

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact model, related to User
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.owner} : {self.subject}"
