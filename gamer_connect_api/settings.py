665f976fbbe98664c70c53188f8a91c6970c1b3e
JamieB92 <j.bonner6992@gmail.com>
2023-12-11 13:23:34 +0000

    turn back on local database

--- gamer_connect_api/settings.py
+++ gamer_connect_api/settings.py
@@ -150,17 +150,17 @@ WSGI_APPLICATION = 'gamer_connect_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# if 'DEV' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#      }
-# else:
-DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+     }
+else:
+    DATABASES = {
+            'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        }
 
 
 # Password validation
